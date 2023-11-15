3ffabe932cf352e2b9b71bf29f8050c62fb09552
kevindasilva1603 <kevinsilva1603@gmail.com>
2023-11-15 10:41:39 +0100

    First

---
+++ script.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', function () {
+    loadFormData()
+    generatePackingList()
+})
+
+document
+    .getElementById('packingForm')
+    .addEventListener('submit', function (event) {
+        event.preventDefault()
+        saveFormData()
+        generatePackingList()
+    })
+
+document.getElementById('resetButton').addEventListener('click', function () {
+    resetForm()
+    this.style.display = 'none'
+})
+
+function saveFormData() {
+    const nights = document.getElementById('nights').value
+    const climate = document.getElementById('climate').value
+    localStorage.setItem('nights', nights)
+    localStorage.setItem('climate', climate)
+}
+
+function loadFormData() {
+    const savedNights = localStorage.getItem('nights')
+    const savedClimate = localStorage.getItem('climate')
+
+    if (savedNights) {
+        document.getElementById('nights').value = savedNights
+    }
+    if (savedClimate) {
+        document.getElementById('climate').value = savedClimate
+    }
+}
+
+function resetForm() {
+    document.getElementById('nights').value = 3
+    document.getElementById('climate').value = 'modere'
+    document.getElementById('packingList').innerHTML = ''
+    localStorage.removeItem('nights')
+    localStorage.removeItem('climate')
+}
+
+function generatePackingList() {
+    const nights = document.getElementById('nights').value
+    const climate = document.getElementById('climate').value
+    let listItems = [
+        { label: 'Un sac à dos/valise' },
+        { label: `${Math.min(nights, 10)} paire(s) de chaussettes` },
+        { label: `${Math.min(nights, 10)} sous-vêtement(s)` },
+        { label: `${Math.min(nights, 10)} t-shirt(s)` },
+        { label: `${nights < 4 ? 1 : nights <= 6 ? 2 : 3} pull(s)/sweat(s)` },
+        { label: `${nights < 4 ? 1 : nights <= 6 ? 2 : 3} pantalon(s)` },
+    ]
+
+    if (climate !== 'chaud') {
+        listItems.push({ label: 'Une veste' })
+    }
+
+    if (nights > 6) {
+        listItems.push({ label: '1 paire de chaussures' })
+    }
+
+    if (climate === 'chaud') {
+        listItems.push({ label: 'Tongs/claquettes' })
+        listItems.push({ label: 'Lunettes de soleil' })
+        listItems.push({ label: 'Maillot de bain' })
+    } else if (climate === 'froid') {
+        listItems.push({ label: 'Gants' })
+        listItems.push({ label: 'Bonnet' })
+        listItems.push({ label: 'Écharpe' })
+        listItems.push({ label: 'Chaussettes épaisses' })
+    }
+
+    if (nights > 10) {
+        listItems.push({
+            label: 'Si vous partez en voyage plus de 10 nuits, faites des machines. Voyagez léger.',
+        })
+    }
+
+    const packingList = document.getElementById('packingList')
+    packingList.innerHTML = listItems
+        .map(
+            (item) =>
+                `<div>
+                <input type="checkbox" id="${item.label}" name="${item.label}">
+                <label for="${item.label}">${item.label}</label>
+            </div>`
+        )
+        .join('')
+    document.getElementById('resetButton').style.display = 'block'
+}
